a73b95f10fba3f82eb839618ab801d07d5290fd5
Maek0s <maek0szaho@gmail.com>
2024-12-15 02:18:11 +0100

    (Stable) Bug arreglado de Traducciones
    Había un error con las traducciones al solicitar las traducciones, accedí directamente al enlace y ya (Cuidado con cambios de nombres rompería las traducciones de nuevo)

--- scripts/scriptTraducciones.js
+++ scripts/scriptTraducciones.js
@@ -22,7 +22,6 @@ document.getElementById('english-flag').addEventListener('click', function() {
 
 // Función para cargar el archivo JSON del idioma
 async function loadTranslations(language) {
-//    const response = await fetch(`../traducciones/${language}.json`);
     const response = await fetch(`https://raw.githubusercontent.com/Maek0s/PokeSharpWeb/main/traducciones/${language}.json`);
 
     console.log(`../traducciones/${language}.json`);
