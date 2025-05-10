fcdc1340461056aab33a31403c6ee5e6787c51f2
Maek0s <maek0szaho@gmail.com>
2025-05-10 18:35:18 +0200

    Alpha released y descargable

--- scripts/scriptsHome.js
+++ scripts/scriptsHome.js
@@ -1,4 +1,3 @@
 function download() {
-    alert(`Todavía no esta disponible, se encuentra en desarrollo.
-Para más información accede al discord, lo tendrás por aquí ->`)
+    window.open("https://github.com/Maek0s/PokeSharp/releases/tag/v1.0.0-apha", "_blank");
 }
