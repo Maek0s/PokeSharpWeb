9b92b218f44a70e705cf420f9371985e9e030883
Maek0s <maek0szaho@gmail.com>
2025-06-01 19:05:47 +0200

    Cambio en el botón de descarga

--- scripts/scriptsHome.js
+++ scripts/scriptsHome.js
@@ -1,3 +1,3 @@
 function download() {
-    window.open("https://github.com/Maek0s/PokeSharp/releases/tag/v1.0.0-apha", "_blank");
+    window.open("https://github.com/Maek0s/PokeSharp/releases", "_blank");
 }
